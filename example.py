6881a87e42f21306cd1c191c5f31c82f5724c3df
yrc17 <82015000+yrc17@users.noreply.github.com>
2023-02-05 00:38:24 +0800

    Add files via upload

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
 """ This is an example for the application of PILOT"""
-from pilot import Pilot, Tree
+from pilot import Pilot
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 
 # import the dataset
 path = "https://archive.ics.uci.edu/ml/machine-learning-databases/00514/Bias_correction_ucl.csv"
@@ -19,18 +21,44 @@ temp.station = temp.station.astype(int)
 
 # define the predictors and the response
 X, y = (
-            temp.drop(["Next_Tmax", "Next_Tmin"], axis=1).values,
-            temp["Next_Tmax"].values,
-        )
+    temp.drop(["Next_Tmax", "Next_Tmin"], axis=1).values,
+    temp["Next_Tmax"].values,
+)
+
+# split the dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # initialize the model
 model = Pilot.PILOT()
 
-# fit the dataset
-model.fit(X,y)
+# fit on the training data
+model.fit(X_train, y_train)
 
-# make predictions
-pred = np.mean((model.predict(x=X) - y) ** 2)
-print(pred)
+# make predictions and compute mse
+pred_train = model.predict(x=X_train) 
+mse_train = np.mean((pred_train - y_train) ** 2)
+print(f"The mse on the training data is {mse_train}.")
+pred_test = model.predict(x=X_test) 
+mse_test = np.mean((pred_test - y_test) ** 2)
+print(f"The mse on the test data is {mse_test}.")
 
+# make prediction v.s. response plots on the training data
+plt.scatter(pred_train, y_train, alpha = 0.3, s = 15)
+# make the reference line such that pred = true response
+x_grid = np.linspace(start = 18, stop = 38, num = 401)
+plt.plot(x_grid, x_grid, linestyle = 'dashed', color = 'r')
+plt.title('Prediction v.s. Response on Training Data')
+plt.xlabel('Prediction')
+plt.ylabel('y_train')
+plt.savefig('train_scatter.jpg')
+plt.close()
 
+# make prediction v.s. response plots on the test data
+plt.scatter(pred_test, y_test, alpha = 0.3, s = 15)
+# make the reference line such that pred = true response
+plt.plot(x_grid, x_grid, linestyle = 'dashed', color = 'r')
+plt.title('Prediction v.s. Response on Test Data')
+plt.xlabel('Prediction')
+plt.ylabel('y_test')
+plt.savefig('test_scatter.jpg')
+plt.close()
